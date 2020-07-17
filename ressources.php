5dc11ee47abfcd15f47f27969fd4e0cd24305980
nathanportier <nportiern@gmail.com>
2020-07-17 09:35:34 +0200

    Create ressources.php

---
+++ ressources.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+
+<html>
+
+<?php include("Back-office/header.php")?>;
+
+<header>
+    <?php include("Back-office/nav.php")?>; <br>
+    <h1> Summer Code Camp </h1>
+</header>
+
+<body>
+    <div id="texte"><br>
+        <h2>RESSOURCES</h2><br>
+
+        <div class="source">
+
+            <table>
+                <th id="titreressource"> Nom du site</th>
+                <th id="titreressource">Lien</th>
+                <th id="titreressource">Commentaire</th>
+
+                <tr>
+                    <td>Codecademy</td>
+                    <td> <a href="https://www.codecademy.com/learn/learn-html" target="_blank" class="link">https://www.codecademy.com/learn/learn-html</a></td>
+                    <td>CSS</td>
+
+                </tr>
+
+                <tr>
+                    <td>Developpez</td>
+                    <td><a href="https://pbnaigeon.developpez.com/tutoriel/CSS-HTML/notions-de-base" target="_blank" class="link">https://pbnaigeon.developpez.com/tutoriel/CSS-HTML/notions-de-base</a>
+                    </td>
+                    <td>HTML</td>
+                </tr>
+                <tr>
+                    <td>OpenClassrooms</td>
+                    <td><a href="https://openclassrooms.com/fr/" target="_blank" class="link">https://openclassrooms.com/fr/</a>
+                    </td>
+                    <td>HTML</td>
+                </tr>
+            </table>
+            <br>
+
+        </div>
+
+        <div id="photos">
+            <img src="assets/IMG_0414.JPG" />
+    
+        </div>
+
+</body>
+
+<?php include("Back-office/footer.php")?>;
+
+</html>
