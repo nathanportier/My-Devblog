a61fb748412442d681683c925de20f7f881fc2bd
nathanportier <nportiern@gmail.com>
2020-07-17 09:35:19 +0200

    Create medias.php

---
+++ medias.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+
+<?php include("Back-office/header.php")?>;
+
+<header>
+<?php include("Back-office/nav.php")?>; <br>
+    <h1> Summer Code Camp </h1>
+</header>
+
+<body>
+    <div id="medias">
+        <h2>MEDIAS</h2><br>
+        <h3> Tristam </h3>
+        <audio controls src="assets/tristram.mp3" src="assets/tristram.ogg"></audio>
+
+        <h4> Pink
+            Fluffy Unicorn Dancing On Rainbows </h4><br>
+        <video controls src="./assets/pink_fluffy_unicorn.mp4">Pink
+            Fluffy Unicorn Dancing On Rainbows”</video><br>
+
+    </div>
+
+</body>
+<?php include("Back-office/footer.php")?>;
+
+</html>
