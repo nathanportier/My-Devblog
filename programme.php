0a2efbb5ad1b0e12104eeaf5a4c5ed4a900ca59b
nathanportier <nportiern@gmail.com>
2020-07-17 09:35:31 +0200

    Create programme.php

---
+++ programme.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+
+<html>
+
+<?php include("header.php")?>;
+
+<header>
+<?php include("nav.php")?>; <br>
+    <h1> Summer Code Camp </h1>
+</header>
+
+<body>
+    <h2>PROGRAMME</h2>
+    <br>
+    <div id="tableau">
+        <table>
+            <tr>
+                <th>DATE</th>
+                <th>PROGRAMME</th>
+            </tr>
+            <tr>
+                <td>Jour 1</td>
+                <td>Accueil des apprenants, découverte des outils, configuration des machines, conférence "Fonctionnement et architecture d'internet" et Sandbox</td>
+            </tr>
+            <tr>
+                <td>Jour 2</td>
+                <td>HTML-1</td>
+            </tr>
+            <tr>
+                <td>Jour 3</td>
+                <td>HTML-2</td>
+            </tr>
+            <tr>
+                <td>Jour 4</td>
+                <td>HTML-3</td>
+            </tr>
+            <tr>
+                <td>Jour 5</td>
+                <td>PHP- 1 & 2</td>
+            </tr>
+            <tr>
+                <td><br></td>
+                <td><br></td>
+            </tr>
+            <tr>
+                <td><br></td>
+                <td><br></td>
+            </tr>
+            <tr>
+                <td>Jour 8</td>
+                <td>PHP-3 & 4</td>
+            </tr>
+            <tr>
+                <td>Jour 9</td>
+                <td>PHP-5 & 6</td>
+            </tr>
+            <tr>
+                <td>Jour 10 <br></td>
+                <td>PHP-7 & 8</td>
+            </tr>
+            <tr>
+                <td>Jour 11 </td>
+                <td>PHP-9 & 10</td>
+            </tr>
+            <tr>
+                <td>Jour 12</td>
+                <td>PHP-11</td>
+            </tr>
+            <tr>
+                <td><br></td>
+                <td><br></td>
+            </tr>
+            <tr>
+                <td><br></td>
+                <td><br></td>
+            </tr>
+            <tr>
+                <td>Jour 15</td>
+                <td>SQL- 1 & 2</td>
+            </tr>
+            <tr>
+                <td>Jour 16</td>
+                <td>SQL-3</td>
+            </tr>
+            <tr>
+                <td>Jour 17</td>
+                <td>ERP de comptabilité (my_compta)</td>
+            </tr>
+            <tr>
+                <td>Jour 18</td>
+                <td>ERP de comptabilité (my_compta)</td>
+            </tr>
+            <tr>
+                <td>Jour 19</td>
+                <td>ERP de comptabilité (my_compta)</td>
+            </tr>
+            <tr>
+                <td><br></td>
+                <td><br></td>
+            </tr>
+            <tr>
+                <td><br></td>
+                <td><br></td>
+            </tr>
+            <tr>
+                <td>Jour 22</td>
+                <td>Soutenance my_compta</td>
+            </tr>
+            <tr>
+                <td>Jour 23</td>
+                <td>Initiation VBA</td>
+            </tr>
+            <tr>
+                <td>Jour 24</td>
+                <td>Soutenance VBA</td>
+            </tr>
+            <tr>
+                <td>Jour 25</td>
+                <td>Projet libre fullstack from scratch</td>
+            </tr>
+            <tr>
+                <td>Jour 26</td>
+                <td>Soutenance projet</td>
+            </tr>
+            <tr>
+                <td>Jour 27</td>
+                <td>26 ans</td>
+            </tr>
+            <tr>
+                <td>Jour 28</td>
+                <td>26 ans</td>
+            </tr>
+            <tr>
+                <td>Jour 29</td>
+                <td>26 ans</td>
+            </tr>
+        </table>
+    </div>
+
+
+</body>
+<?php include("Back-office/footer.php")?>;
+
+</html>
