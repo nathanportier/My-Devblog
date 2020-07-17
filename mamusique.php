34d393e1b95953608889e433e3cecaac685d8264
nathanportier <nportiern@gmail.com>
2020-07-17 09:35:12 +0200

    Create mamusique.php

---
+++ mamusique.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./Style/style.css">
+    <title></title>
+</head>
+
+<body>
+    
+    <?php include("Back-office/header.php")?>;
+   <?php include("Back-office/nav.php")?>;   
+    <div id="text">
+        <h2><br>Mes sons préférés du moment</h2><br>
+        
+        
+<iframe width="720" height="400" src="https://www.youtube.com/embed/9eCzi_LGz0U" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>
+        <iframe width="720" height="400" src="https://www.youtube.com/embed/vfoa59DOTyc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        
+    </div>
+
+        <div>
+            <br><div class=carre><br>
+                <h3>Instagram</h3>
+                <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/242c45218bb4505e9d48edc949c8a1c8.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
+
+                <a class="twitter-timeline" href="https://twitter.com/wymathiieu?ref_src=twsrc%5Etfw">Mathieu</a>
+                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </div>
+            <br clear="both" />
+        </div>
+
+        <br>
+<?php include("Back-office/footer.php")?>;
+</body>
+
+</html>
