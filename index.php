1181f65eb0f514deedf23e208228f33bfb411e37
nathanportier <nportiern@gmail.com>
2020-07-17 09:35:01 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html>
+
+<?php include("Back-office/header.php")
+?>;
+
+<header>
+<?php include("Back-office/nav.php")
+?>; <br>
+    <h1> MyDevblog </h1>
+</header>
+
+<body>
+   
+    <div id="texte">
+        <h2>ACCUEIL</h2><br>
+        <p> Le numérique, nouvelle ère de l’humanité, source d’innovation et de performance ?
+            Pour mieux comprendre cette révolution, venez apprendre les rudiments du code et
+            découvrir les bases de la programmation des sites internet. Destiné aux étudiants,
+            ce code camp vous initiera en un mois aux principaux langages de programmation
+            Web (HTML-CSS et PHP), vous dévoilera le fonctionnement d’outils comme Google et
+            Paypal et vous éclaira sur la sécurité des sites internet.</p>
+
+    </div>
+<br>
+    <div id="photos">
+        <img src="assets/IMG_0414.JPG" />
+       
+    </div>
+
+</body>
+<?php include("Back-office/footer.php")?>;
+
+</html>
