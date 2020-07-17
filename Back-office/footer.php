870d456b3ef33a0afe6fafeece29f5b6212eeffd
nathanportier <nportiern@gmail.com>
2020-07-17 09:35:53 +0200

    Create footer.php

---
+++ Back-office/footer.php
@@ -0,0 +1,10 @@
+<br>
+<br><div id="write">
+            <p><strong> - 25 rue du Casterneau - nathan.portier@iseg.fr</strong></p>
+        </div>
+<footer>
+       <div id="icons">
+        <br> <a href="http://www.instagram.com/bugsy_karl_malone/" target="_blank">
+            <img class="foot" src="./assets/insta.png"></a><br>
+        </div>
+    </footer>
